23e066f09090314be7b0e339eeb417e1c5f6164d
diegopuertolas <diegopuertolasruiz@gmail.com>
2025-11-11 10:39:18 +0100

    Final project

--- src/app.js
+++ src/app.js
@@ -1,100 +1,110 @@
-// BACKEND
+// cities-api-db
 
 const express = require('express');
-const knex = require('knex');
- 
+const { db } = require('./database.js');
+
 const app = express();
 app.use(express.json());
 
-const db = knex({
-    client: 'sqlite3',
-    connection: {
-        filename: 'cities.db'
-    },
-    useNullAsDefault: true
-})
-
-// Nota: req = request (lo que envía el cliente), res = response (lo que responde el servidor).
-
-// Definimos una ruta GET en la URL /cities
 app.get('/cities', async (req, res) => {
     
-    const cities = await db('cities').select("*");
+    // TODO Añadir soporte para filtros
+    const cities = await db('cities').select('*');
 
     res.status(200).json(cities);
 });
 
-// Definimos una ruta GET en la URL /cities/:city
 app.get('/cities/:id', async (req, res) => {
     
     const id = req.params.id;
-    //TODO Comprobar que la ciudad existe y devolver un 404 si no está.
-    // TODO Añadir de filtrar por nombre de ciudad.
-
     const city = await db('cities').select('*').where({id: id}).first();
+    
+    if (city == null) {
+        return res.status(404).json({
+            code: 404,
+            title: 'not-found',
+            message: 'the city does not exist'
+        });
+    }
 
     res.status(200).json(city);
 });
 
-// Definimos una ruta POST en la URL /cities
-app.post('/cities', async (req, res) => {
+app.post('/cities/:id', async (req, res) => {
     
-    // Extraemos los valores enviados en el cuerpo de la petición (JSON)
-    const name = req.body.name; // Obtenemos el nombre de la ciudad.
-    //TODO Comprobar que altitude y population son de tipo entero.
-    const altitudeValue = req.body.altitude; // Obtenemos el valor de la altitud.
-    const populationValue = req.body.population; // Obtenemos el valor de la población.
-    const capital = req.body.capital; // Obtenemos el valor de la capital.
+    const id = req.params.id;
+    const city = await db('cities').select('*').where({id: id}).first();
+    
+    if (city != null) {
+        return res.status(409).json({
+            code: 409,
+            title: 'conflict',
+            message: 'a city already exists with that name'
+        });
+    }
 
-    // Guardamos la ciudad en el objeto "cities", usando el nombre como clave.
-    // Ejemplo: cities["Madrid"] = { altitude: 667, population: 3223000 }
-    const newCity = await db('cities').insert ({
+    // TODO Comprobar que altitude y population son de tipo entero
+    const altitude = req.body.altitude;
+    const population = req.body.population;
+    const capital = req.body.capital;
+
+    const newCity = await db('cities').insert({
         name: name,
-        altitude: altitudeValue,
-        population: populationValue,
+        altitude: altitude,
+        population: population,
         capital: capital
     });
 
-    // Respondemos al cliente con un estado HTTP 201 (Created).
-    // Esto significa que el recurso (la ciudad) se creó con éxito.
+    // TODO Devolver todos los datos de la ciudad como respuesta
     res.status(201).json(newCity);
 });
 
-// Definimos una ruta PUT en la URL /cities/:city
-app.put('/cities/:id', async (req, res) => {
-
-    const id = req.params.city;
-    //TODO Comprobar si existe la ciudad.
+app.delete('/cities/:id', async (req, res) => {
     
-    // Datos de altitud y población que llegan desde el cuerpo.
-    const name = req.body.name
-    const altitudeValue = req.body.altitude; // Obtenemos el valor de la altitud.
-    const populationValue = req.body.population; // Obtenemos el valor de la población.
-    const capital = req.body.capital; // Obtenemos el valor de la capital.
+    const id = req.params.id;
+    const city = await db('cities').select('*').where({id: id}).first();
+    
+    if (city == null) {
+        return res.status(404).json({
+            code: 404,
+            title: 'not-found',
+            message: 'the city does not exist'
+        });
+    }
+    await db('cities').where({id: id}).del();
+
+    res.status(204).end();
+});
+
+app.put('/cities/:id', async (req, res) => {
+    
+    const id = req.params.id;
+    
+    const city = await db('cities').select('*').where({id: id}).first();
+    if (city == null) {
+        return res.status(404).json({
+            code: 404,
+            title: 'not-found',
+            message: 'the city does not exist'
+        });
+    }
+
+    const name = req.body.name;
+    // TODO Validar alguna cosa
+    const altitude = req.body.altitude;
+    const population = req.body.population;
+    const capital = req.body.capital;
 
     await db('cities').where({id: id}).update({
         name: name,
-        altitude: altitudeValue,
-        population: populationValue,
+        altitude: altitude,
+        population: population,
         capital: capital
     });
 
     res.status(204).end();
- 
-})
- 
-// Definimos una ruta DELETE en la URL /cities/:city
-app.delete('/cities/:id', async (req, res) => {
-
-    const id  = req.params.id;
-    //TODO Comprobar que la ciudad existe.
-    
-    await db('cities').where({id: id}).del();
-
-    res.status(204).end();
-
 });
- 
+
 app.listen(8080, () => {
-    console.log('Iniciando el backend en el puerto 8080');
+    console.log("Iniciando el backend en el puerto 8080");
 });
